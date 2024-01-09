5227072651c8d94c3a6b7d2fd674a4c8a74c9b0a
Cyprien <cypr.jeux@gmail.com>
2024-01-09 10:27:00 +0100

    header/footer add

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Accueil</title>
-    <meta name="viewport" content="width=device-width">
-</head>
+<x-layout>
 
 <div class="Content">       
     <input type="text" id="name" name="name" required minlength="4" maxlength="80" size="10" />
@@ -14,3 +7,5 @@
 </div>
 
 <script src="#"></script>
+
+</x-layout>
