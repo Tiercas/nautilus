63e48584f1b5401b69d93d5f5ede2bfbb2ecf057
Cyprien <cypr.jeux@gmail.com>
2024-01-09 10:11:32 +0100

    start of php login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+    <meta name="viewport" content="width=device-width">
+</head>
+
+<div class="Content">       
+
+
+</div>
+
+<script src="#"></script>
