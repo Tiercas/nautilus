0096af692c4af6ee1c18673d06e8f2b52275d07f
LucBUTGH <lucleveque14@outlook.fr>
2024-01-09 17:44:04 +0100

    fix: center divers tab divings

--- resources/views/alldiversactivities.blade.php
+++ resources/views/alldiversactivities.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
     <x-page-title>Plongées réalisées</x-page-title>
-    <div class="shadow-md max-w-full rounded-lg overflow-hidden border-2">
-        <table class="text-sm text-left text-gray-500 w-full">
+    <div class="shadow-md max-w-full w-2/3 content-between rounded-lg overflow-hidden border-2 mx-auto">        <table class="text-sm text-left text-gray-500 w-full">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
                 <th scope="col" class="px-6 py-3">Nom</th>
