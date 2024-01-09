5d469238282576a7036cae987196dd7df35771ba
Alenso Lopes <alenso.lopes@gmail.com>
2024-01-09 15:08:06 +0100

    feat: footer w/ shape & text

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,15 @@
-<footer>
-  footer
+<footer class="w-full flex flex-row sm:grid sm:grid-cols-3 bg-[#002550] h-36 items-center">
+  <div class='w-full bg-[#2A4192]  h-full flex flex-row items-center px-4'>
+    <img src='../../img/logo.png' class="h-[90%] w-full" alt=''>
+  </div>
+  <div class='text-white bg-[#2A4192] h-full w-1/2 justify-end'
+      style="clip-path: ellipse(75% 75% at 0 33%);">
+    </div>
+  <div class='md:flex md:flex-col items-start w-full md:px-12 '>
+    <ul class='text-white text-md'>
+      <li class=''>Nous Contacter !</li>
+      <li class=''>Email - contact@Tiercas.fr</li>
+      <li class=''>Tel - 02 30 30 03 30</li>
+    </ul>
+  </div>
 </footer>
