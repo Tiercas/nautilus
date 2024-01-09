69b7ee05a9a83abada86b00905979d9e2b752a11
Julien Sailly <juliensailly76@gmail.com>
2024-01-09 20:15:06 +0100

    feat: adding the middleware for api

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dives/{id}/divers', [DivesAPI::class, 'getDivers']);
+Route::middleware('auth:sanctum')->get('/dives/{id}/divers', [DivesAPI::class, 'getDivers']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
