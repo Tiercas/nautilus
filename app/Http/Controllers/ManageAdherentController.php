e4be2491f2b54546e07ed177e10c0bd1ad025067
Julien Sailly <juliensailly76@gmail.com>
2024-01-10 15:51:05 +0100

    feat: modification des roles via la page gestion des adherents fonctionnelle

--- app/Http/Controllers/ManageAdherentController.php
+++ app/Http/Controllers/ManageAdherentController.php
@@ -8,9 +8,11 @@ use Illuminate\Http\Request;
 
 class ManageAdherentController extends Controller {
     function index() {
-        $users = User::select('*')->join('car_role_attribution', 'car_role_attribution.US_ID', '=', 'car_user.US_ID')->orderBy('car_user.US_ID')->get();
+        $users = User::select('car_user.US_ID', 'car_user.US_FIRST_NAME', 'car_user.US_NAME', 'car_role_attribution.ROL_CODE')->leftjoin('car_role_attribution', 'car_role_attribution.US_ID', '=', 'car_user.US_ID')->orderBy('car_user.US_ID')->get();
+
         $user_role = array();
         $usersInfos = array();
+
         foreach ($users as $user) {
             $usersInfos[$user->US_ID] = [$user->US_ID, $user->US_FIRST_NAME, $user->US_NAME];
             if (!array_key_exists($user->US_ID, $user_role)){
@@ -19,19 +21,19 @@ class ManageAdherentController extends Controller {
             $user_role[$user->US_ID][] = $user->ROL_CODE;
         }
         $users = [$usersInfos, $user_role];
+
         return view('manageAdherent', ['users' => $users]);
     }
 
     function update(Request $request) {
-        @dd($request->all());
         $uptUser = $request->all();
+        RoleAttribution::truncate();
         foreach ($uptUser as $key => $value) {
             if ($key != '_token') {
-                $user = User::find($key);
-                $key =
-                $userRole = RoleAttribution::select('*')->where('US_ID', '=', $key)->get();
-                $user->ROL_CODE = $value;
-                $user->save();
+                $roleAttribution = new RoleAttribution();
+                $roleAttribution->US_ID = explode("_", $key)[1];
+                $roleAttribution->ROL_CODE = $value;
+                $roleAttribution->save();
             }
         }
         return redirect()->route('manage_members');
@@ -39,3 +41,4 @@ class ManageAdherentController extends Controller {
 
 
 }
+
--- resources/views/manageAdherent.blade.php
+++ resources/views/manageAdherent.blade.php
@@ -14,9 +14,9 @@
                 </tr>
                 </thead>
                 <tbody>
-                @for ($i = 1; $i < count($users[0]); $i++)
+                @foreach ($users[0] as $i => $value)
                     <tr class="odd:bg-white even:bg-gray-50 border-b">
-                        <td class="px-6 py-4">{{ $users[0][$i][1] . " " . $users[0][$i][2] }}</td>
+                        <td class="px-6 py-4">{{ $users[0][$i][1] . " " . strtoupper($users[0][$i][2]) }}</td>
                         <td class="px-6 py-4">
                             <input type="checkbox" id="checkbox{{ $users[0][$i][0] }}" name="checkbox_{{ $users[0][$i][0] }}_DIV" value="DIV" @if(array_search('DIV', $users[1][$i]) !== false) checked @endif>
                         </td>
@@ -30,7 +30,7 @@
                             <input type="checkbox" id="checkbox{{ $users[0][$i][0] }}" name="checkbox_{{ $users[0][$i][0] }}_DIR" value="DIR" @if(array_search('DIR', $users[1][$i]) !== false) checked @endif>
                         </td>
                     </tr>
-                @endfor
+                @endforeach
                 </tbody>
             </table>
 
