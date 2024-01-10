6ddb7df3b4469e2c1b26ba2f6b204f2d90730abf
Waterfox <waterfox80@gmail.com>
2024-01-10 15:57:57 +0100

    Inscription possible  only if level can

--- app/Http/Controllers/DivesList.php
+++ app/Http/Controllers/DivesList.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\DivingSession;
+use App\Models\Prerogative;
+use App\Models\User;
+use Error;
 use Illuminate\View\View;
 
 class DivesList extends Controller
 {
     function index(): View
     {
-        $dives = DivingSession::select('DS_CODE', 'CAR_DIVING_SESSION.DL_ID', 'DS_DATE', 'DL_DEPTH', 'CAR_SCHEDULE', 'DL_NAME')
+        $dives = DivingSession::select('DS_CODE', 'CAR_DIVING_SESSION.DL_ID', 'DS_DATE', 'DL_DEPTH', 'CAR_SCHEDULE', 'DL_NAME', 'DS_MAX_DEPTH')
             ->join('CAR_DIVING_LOCATION', 'CAR_DIVING_SESSION.DL_ID', '=', 'CAR_DIVING_LOCATION.DL_ID')
             ->get();
-        return view('dives', ['dives' => $dives]);
+        $user = session()->get('user');
+        return view('dives', ['dives' => $dives, 'userPre' => Prerogative::find($user->PRE_CODE)]);
     }
 }
