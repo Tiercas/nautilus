0dbbc2001a4a6ed3c1742ce7206c4cfd03b5227f
Victor Friboulet <victor.friboulet@etu.unicaen.fr>
2024-01-10 10:25:25 +0100

    fix: conflicts and user_id in registration

--- app/Models/DivingSignUpModel.php
+++ app/Models/DivingSignUpModel.php
@@ -8,12 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class DivingSignUpModel extends Model
 {
     protected $table = 'CAR_REGISTRATION';
-<<<<<<< HEAD
-    protected $primaryKey = ['us_id', 'ds_code'];
-=======
-    protected $primaryKey = ['US_ID', 'DS_CODE'];
->>>>>>> diversbysession
-    protected $keyType = ['int', 'string'];
     protected $fillable = ['US_ID', 'DS_CODE', 'REG_ACTIVE'];
     use HasFactory;
 }
