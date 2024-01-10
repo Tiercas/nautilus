394fe95e0447150f565863a702a52ef44c8adce1
Waterfox <waterfox80@gmail.com>
2024-01-10 22:12:19 +0100

    Add DS_ACTIVE filter and sort dives by date

--- app/Http/Controllers/DivesList.php
+++ app/Http/Controllers/DivesList.php
@@ -12,8 +12,10 @@ class DivesList extends Controller
 {
     function index(): View
     {
-        $dives = DivingSession::select('DS_CODE', 'CAR_DIVING_SESSION.DL_ID', 'DS_DATE', 'DL_DEPTH', 'CAR_SCHEDULE', 'DL_NAME', 'DS_MAX_DEPTH')
+        $dives = DivingSession::select('DS_CODE', 'DS_ACTIVE', 'CAR_DIVING_SESSION.DL_ID', 'DS_DATE', 'DL_DEPTH', 'CAR_SCHEDULE', 'DL_NAME', 'DS_MAX_DEPTH')
             ->join('CAR_DIVING_LOCATION', 'CAR_DIVING_SESSION.DL_ID', '=', 'CAR_DIVING_LOCATION.DL_ID')
+            ->where('DS_ACTIVE', 1)
+            ->orderBy('DS_DATE', 'asc')
             ->get();
         $user = session()->get('user');
         return view('dives', ['dives' => $dives, 'userPre' => Prerogative::find($user->PRE_CODE)]);
