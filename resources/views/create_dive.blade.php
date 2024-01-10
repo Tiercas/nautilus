54f51b0b585995abf432ab56e207a42694f291bf
Julien Sailly <juliensailly76@gmail.com>
2024-01-10 21:19:20 +0100

    fix: orth

--- resources/views/create_dive.blade.php
+++ resources/views/create_dive.blade.php
@@ -177,7 +177,7 @@
             </div>
             @if(isset($precedent))
                 <div id="toast-default" class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-                    <div class="ms-3 text-sm font-normal">Plongée crée !</div>
+                    <div class="ms-3 text-sm font-normal">Plongée créée !</div>
                     <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-default" aria-label="Close">
                         <span class="sr-only">Close</span>
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
