3d6fc40949b0c21f8750b5fd609fc595ee771af5
Guilhem <gsaintgaudin@gmail.com>
2024-01-11 18:24:20 +0100

    made this shit accessible from the app

--- resources/views/dives_list_management.blade.php
+++ resources/views/dives_list_management.blade.php
@@ -28,13 +28,15 @@
                                 </svg>
                             </x-button>
                         </a>
-                        <x-button diveId="dropdownButton-{{$dive->DS_CODE}}">
-                            <svg class="w-6 h-6 text-gray-800 text-white" aria-hidden="true"
-                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                <path
-                                    d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                            </svg>
-                        </x-button>
+                        <a href="/modificationdives/members/{{$dive['DS_CODE']}}">
+                            <x-button diveId="dropdownButton-{{$dive->DS_CODE}}">
+                                <svg class="w-6 h-6 text-gray-800 text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path
+                                        d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                                </svg>
+                            </x-button>
+                        </a>
                     </td>
                 </tr>
                 <x-drop-down id="dropdown{{$dive->DS_CODE}}">
