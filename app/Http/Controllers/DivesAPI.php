ac51b1e9fcb3d3f52056a197fd0e54fe84bd5b2e
Julien Sailly <juliensailly76@gmail.com>
2024-01-11 13:47:32 +0100

    feat: adding DG_NUMBER

--- app/Http/Controllers/DivesAPI.php
+++ app/Http/Controllers/DivesAPI.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\DivingSession;
+use App\Models\User;
 
 class DivesAPI extends Controller
 {
     function getDivers($diveID): array
     {
-        return DivingSession::find($diveID)->getParticipants();
+        $participants = User::select('*')->join('CAR_REGISTRATION', 'CAR_REGISTRATION.US_ID', '=', 'CAR_USER.US_ID')->where('CAR_REGISTRATION.DS_CODE', $diveID);
+        $participants = $participants->get()->toArray();
+        return $participants;
     }
 }
