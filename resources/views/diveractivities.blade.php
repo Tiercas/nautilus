cadd7feafb94d3d3826b762406e0826eea8279af
cyprien <cyprien.delapoeze-dharambure@etu.unicaen.fr>
2024-01-11 14:25:47 +0100

    divings update

--- resources/views/diveractivities.blade.php
+++ resources/views/diveractivities.blade.php
@@ -2,23 +2,8 @@
     @if (!session()->has('user'))
         Vous devez être connecté pour accéder à cette page.
     @else
-        <x-page-title hrSize="70%">Historique des plongées de
-            {{ session('user')->US_FIRST_NAME . ' ' . session('user')->US_NAME }}</x-page-title>
-
-        <div class="shadow-md max-w-full w-1/3 rounded-lg overflow-hidden border-2 mx-auto">
-            <table class="text-sm text-left text-gray-500 w-full">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                    <tr>
-                        <th scope="col" class="px-12 py-3">Nombre de plongées</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="odd:bg-white even:bg-gray-50 border-b">
-                        <td class="px-12 py-4">{{ $usersCount }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <x-page-title>Plongées de
+            {{ session('user')->US_FIRST_NAME . ' ' . session('user')->US_NAME }} | {{ $usersCount }} plongées restantes</x-page-title>
 
         <p style="font-size: 35px"> A venir </p>
         <hr style="height: 3px;background-color: black;margin-bottom: 30px;margin-top: 5px; width : 20%;">
