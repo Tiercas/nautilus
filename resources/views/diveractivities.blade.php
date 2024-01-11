17f593cf4e7aeba1244ac96b3a34c7dc16161919
LucBUTGH <lucleveque14@outlook.fr>
2024-01-11 14:09:01 +0100

    fix: number dives title

--- resources/views/diveractivities.blade.php
+++ resources/views/diveractivities.blade.php
@@ -2,23 +2,9 @@
     @if (!session()->has('user'))
         Vous devez être connecté pour accéder à cette page.
     @else
-        <x-page-title hrSize="70%">Historique des plongées de
-            {{ session('user')->US_FIRST_NAME . ' ' . session('user')->US_NAME }}</x-page-title>
+        <x-page-title hrSize="70%">Plongées de
+            {{ session('user')->US_FIRST_NAME . ' ' . session('user')->US_NAME }} | Plongées restantes : {{$usersCount}}</x-page-title>
 
-        <div class="shadow-md max-w-full w-1/3 rounded-lg overflow-hidden border-2 mx-auto">
-            <table class="text-sm text-left text-gray-500 w-full">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                    <tr>
-                        <th scope="col" class="px-12 py-3">Nombre de plongées</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="odd:bg-white even:bg-gray-50 border-b">
-                        <td class="px-12 py-4">{{ $usersCount }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
 
         <x-page-subtitle> A venir </x-page-subtitle>
         <div class="mt-10 shadow-md max-w-full w-1/3 rounded-lg overflow-hidden border-2 mx-auto">
@@ -51,7 +37,7 @@
         </div>
 
         <x-page-subtitle> Ayant eu lieu </x-page-subtitle>
-        <div class="mt-10 shadow-md max-w-full w-1/3 rounded-lg overflow-hidden border-2 mx-auto">
+        <div class="mt-10 mb-10 shadow-md max-w-full w-1/3 rounded-lg overflow-hidden border-2 mx-auto">
             <table class="text-sm text-left text-gray-500 w-full">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
